4894f53b0f4fe4a4d1c4eb6e0255b8c0b358aa98
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-01-28 12:01:10 -0500

    snippets: add table

--- _snippets/custom.js
+++ _snippets/custom.js
@@ -96,4 +96,32 @@
   { trigger: "eset", replacement: "\\emptyset", options: "mA" },
   { trigger: "set", replacement: "\\{ $0 \\}$1", options: "mA" },
 
+   {
+    trigger: /table_(\d+)_(\d+)/, replacement: (match) => {
+      const cols = match[1];
+      const rows = match[2];
+
+      let arr = [];
+      for (let i = 0; i <= rows; i++) {
+        arr[i] = [];
+        for (let j = 0; j < cols; j++) {
+
+          if (i == 0 && j == 0) {
+              arr[i][j] = "     ";
+          }
+          else if (i == 1)
+          {
+            arr[i][j] = " --- ";
+          }
+          else
+          {
+            arr[i][j] = " $$  ";
+          };
+        }
+      }
+
+      return arr.map(r => '|' + r.join('|')).join('|\n') + '|';
+
+    }, options: "t", description: "N x N markdown table" // Text mode only
+  },
 ]
