47edd7bd3f3773f55637a9fb2ddd08b259bb27f4
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-02-05 13:45:03 -0500

    snippets/custom: split into more files

---
+++ _snippets/logical.js
@@ -0,0 +1,26 @@
+[
+  { trigger: "or", replacement: "\\lor", options: "mA", priority: -1 },
+  { trigger: "|", replacement: "\\lor$0", options: "mA" }, // This breaks absolute value, so we use abs snippet instead
+
+  { trigger: "and", replacement: "\\land", options: "mA" },
+  { trigger: "&", replacement: "\\land$0", options: "mA" },
+
+  { trigger: "not", replacement: "\\neg", options: "mA" },
+  { trigger: "!", replacement: "\\neg$0", options: "mA" },
+
+  { trigger: "xor", replacement: "\\oplus", options: "mA" },
+
+  { trigger: "imp", replacement: "\\implies", options: "mA" },
+  { trigger: "=>", replacement: "\\implies", options: "mA" },
+
+  { trigger: "exi", replacement: "\\exists", options: "mA" },
+  { trigger: "for", replacement: "\\forall", options: "mA" },
+
+  { trigger: "iin", replacement: "\\in", options: "mA", priority: -1 },
+  { trigger: "nin", replacement: "\\notin", options: "mA" },
+
+  // Existing presets that I don't currently cover
+  { trigger: "<->", replacement: "\\leftrightarrow ", options: "mA" },
+  // { trigger: "!>", replacement: "\\mapsto", options: "mA" }, // NOT WORKING RIGHT NOW
+  { trigger: "=<", replacement: "\\impliedby", options: "mA" },
+]
---
+++ _snippets/sets.js
@@ -0,0 +1,18 @@
+[
+  { trigger: "uni", replacement: "\\cup", options: "mA" },
+  { trigger: "set", replacement: "\\{ $0 \\}$1", options: "mA" },
+
+  { trigger: "sub", replacement: "\\subseteq", options: "mA" },
+  { trigger: "sup", replacement: "\\supseteq", options: "mA" },
+  { trigger: "nsub", replacement: "\\nsubseteq", options: "mA" },
+  { trigger: "nsup", replacement: "\\nsupseteq", options: "mA" },
+
+  { trigger: "psub", replacement: "\\subset", options: "mA" },
+  { trigger: "psup", replacement: "\\supset", options: "mA" },
+  { trigger: "npsub", replacement: "\\not\\subset", options: "mA" },
+  { trigger: "npsup", replacement: "\\not\\supset", options: "mA" },
+
+  { trigger: "notin", replacement: "\\not\\in", options: "mA" },
+  { trigger: "\\\\\\", replacement: "\\setminus", options: "mA" },
+  { trigger: "eset", replacement: "\\emptyset", options: "mA" },
+]
