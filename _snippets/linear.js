9567ff31a01a0c5142f4a845566e7d8f3a57baf8
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-04-29 14:11:41 -0400

    snippets/linear: add comp/prog, spaces in vectors
    `<` now puts spaces around your cursor. Also added `comp` and `proj`,
    which just give you the same in text form, since I don't think there are
    actual latex keywords for them.

--- _snippets/linear.js
+++ _snippets/linear.js
@@ -1,4 +1,6 @@
 [
-{trigger: "<", replacement: "\\langle$0\\rangle $1", options: "mA"},
-{trigger: "len", replacement: "\\lvert\\lvert $1 \\rvert\\rvert $2", options: "mA"},
+  {trigger: "<", replacement: "\\langle $0 \\rangle $1", options: "mA"},
+  {trigger: "len", replacement: "\\lvert\\lvert $1 \\rvert\\rvert $2", options: "mA"},
+  {trigger: "comp", replacement: "\\text{comp}", options: "mA"},
+  {trigger: "proj", replacement: "\\text{proj}", options: "mA"},
 ]
